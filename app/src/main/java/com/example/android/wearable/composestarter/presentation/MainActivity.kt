aab99265f55f7e0173333033611fc8f6a4f35fe8
diegoamezahdz <diego.a.meza.hdz@gmail.com>
2022-06-27 20:54:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/wearable/composestarter/presentation/MainActivity.kt
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.wearable.composestarter.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.*
+import com.example.android.wearable.composestarter.R
+import com.example.android.wearable.composestarter.presentation.theme.WearAppTheme
+
+/**
+ * Simple "Hello, World" app meant as a starting point for a new project using Compose for Wear OS.
+ *
+ * Displays only a centered [Text] composable, and the actual text varies based on the shape of the
+ * device (round vs. square/rectangular).
+ *
+ * If you plan to have multiple screens, use the Wear version of Compose Navigation. You can carry
+ * over your knowledge from mobile and it supports the swipe-to-dismiss gesture (Wear OS's
+ * back action). For more information, go here:
+ * https://developer.android.com/reference/kotlin/androidx/wear/compose/navigation/package-summary
+ */
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            WearApp()
+        }
+    }
+}
+
+@Composable
+fun WearApp() {
+    WearAppTheme {
+
+        val listState = rememberScalingLazyListState()
+        Scaffold(
+            timeText = {
+                if (!listState.isScrollInProgress) {
+                    TimeText()
+                }
+            },
+            vignette = {
+                Vignette(vignettePosition = VignettePosition.TopAndBottom)
+            },
+            positionIndicator = {
+                PositionIndicator(
+                    scalingLazyListState = listState
+                )
+            }
+        ) {
+
+            val contentModifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+            val iconModifier = Modifier.size(24.dp).wrapContentSize(align = Alignment.Center)
+
+            ScalingLazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                autoCentering = AutoCenteringParams(itemIndex = 0),
+                state = listState
+            ) {
+
+                item { ButtonExample(contentModifier, iconModifier) }
+                item { ButtonExamplet(contentModifier, iconModifier) }
+                item { TextExample(contentModifier) }
+                item { CardExample(contentModifier, iconModifier) }
+
+                item { ChipExample(contentModifier, iconModifier) }
+                item { ToggleChipExample(contentModifier) }
+            }
+
+        }
+    }
+}
+//creacion del boton compras
+@Composable
+fun ButtonExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        // Button
+        Button(
+            modifier = Modifier.size(ButtonDefaults.LargeButtonSize),
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.ShoppingCart,
+                contentDescription = "Compras",
+                modifier = iconModifier
+            )
+        }
+    }
+}
+
+//Creación del botón llamada entrante
+@Composable
+fun ButtonExamplet(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        // Button
+        Button(
+            modifier = Modifier.size(ButtonDefaults.LargeButtonSize),
+            onClick = { /* ... */ },
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Phone,
+                contentDescription = "Abrir llamada entrante",
+                modifier = iconModifier
+            )
+        }
+    }
+}
+@Composable
+fun TextExample(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier,
+        textAlign = TextAlign.Center,
+        color = MaterialTheme.colors.primary,
+        text = stringResource(R.string.device_shape)
+    )
+}
+
+//Creacion del Card Mensaje entrante
+@Composable
+fun CardExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    AppCard(
+        modifier = modifier,
+        appImage = {
+            Icon(
+                imageVector = Icons.Rounded.Email,
+                contentDescription = "Abrir mensaje card",
+                modifier = iconModifier
+            )
+        },
+        appName = { Text("Mensajes") },
+        time = { Text("20m") },
+        title = { Text("Diego Meza") },
+        onClick = { /* ... */ }
+    ) {
+        Text("Holaaa!")
+    }
+}
+
+//Creación del chip
+@Composable
+fun ChipExample(
+    modifier: Modifier = Modifier,
+    iconModifier: Modifier = Modifier
+) {
+    Chip(
+        modifier = modifier,
+        onClick = { /* ... */ },
+        label = {
+            Text(
+                text = "5 minutos para llegar",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
+        icon = {
+            Icon(
+                imageVector = Icons.Rounded.Person,
+                contentDescription = "Distancia",
+                modifier = iconModifier
+            )
+        },
+    )
+}
+
+// creación del ToggleChip
+@Composable
+fun ToggleChipExample(modifier: Modifier = Modifier) {
+    var checked by remember { mutableStateOf(true) }
+    ToggleChip(
+        modifier = modifier,
+        checked = checked,
+        toggleControl = {
+            Icon(
+                imageVector = ToggleChipDefaults.switchIcon(checked = checked),
+                contentDescription = if (checked) "On" else "Off"
+            )
+        },
+        onCheckedChange = {
+            checked = it
+        },
+        label = {
+            Text(
+                text = "Sonido",
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    )
+}
